1a1c67315ba82619ad4d6126bea08e4c6b950cee
Loïc Frémont <lc.fremont@gmail.com>
2022-03-28 09:31:04 +0200

    Remove utf8 option

--- tests/DependencyInjection/MobizelSyliusExportExtensionTest.php
+++ tests/DependencyInjection/MobizelSyliusExportExtensionTest.php
@@ -37,34 +37,6 @@ final class MobizelSyliusExportExtensionTest extends AbstractExtensionTestCase
         $this->assertContainerBuilderHasParameter('mobizel.sylius_export_plugin.csv_settings.delimiter', ',');
     }
 
-    /** @test */
-    public function it_registers_settings_utf8_encoding_parameter_with_given_utf8_encoding(): void
-    {
-        $this->setParameter('sylius.resources', []);
-
-        $this->load([
-            'csv_settings' => [
-                'utf8_encoding' => false,
-            ],
-        ]);
-
-        $this->assertContainerBuilderHasParameter('mobizel.sylius_export_plugin.csv_settings.utf8_encoding', false);
-    }
-
-    /** @test */
-    public function it_registers_default_settings_utf8_encoding_parameter(): void
-    {
-        $this->setParameter('sylius.resources', []);
-
-        $this->load([
-            'csv_settings' => [
-                'utf8_encoding' => null,
-            ],
-        ]);
-
-        $this->assertContainerBuilderHasParameter('mobizel.sylius_export_plugin.csv_settings.utf8_encoding', true);
-    }
-
     protected function getContainerExtensions(): array
     {
         return [
